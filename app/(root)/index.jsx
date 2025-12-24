99b80dd7f248b1c5a2e42cfd60e1daaf2a9004ab
umaru <omarprogramer0@gmail.com>
2025-12-24 07:54:32 +0300

    draft

---
+++ app/(root)/index.jsx
@@ -0,0 +1,24 @@
+import { SignedIn, useUser } from '@clerk/clerk-expo';
+import { Text, View } from 'react-native';
+import SignOutButton from "../../components/SignOutButton";
+
+export default function Page() {
+  const { user } = useUser()
+
+  return (
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <SignOutButton />
+      </SignedIn>
+      {/* <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut> */}
+    </View>
+  )
+}
---
+++ app/_layout.jsx
@@ -0,0 +1,17 @@
+import SafeScreen from "@/components/SafeScreen.jsx";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { Slot } from 'expo-router'
+
+export default function RootLayout() {
+  return (
+
+    <ClerkProvider tokenCache={tokenCache}>
+      < SafeScreen >
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
+
+  );
+}
+
