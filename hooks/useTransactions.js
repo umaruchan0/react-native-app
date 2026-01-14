72c9ea600b6205ec6ca398d7c474ecb7240c279f
umaru <omarprogramer0@gmail.com>
2026-01-14 04:49:00 +0300

    app is done

--- hooks/useTransactions.js
+++ hooks/useTransactions.js
@@ -1,7 +1,7 @@
 import { useCallback, useState } from "react";
 import { Alert } from "react-native";
+import { API_URL } from "../constants/api";
 
-const API_URL = "http://192.168.0.104:3000/api";
 
 export const useTransactions = (userId) => {
   const [transactions, setTransactions] = useState([]);
